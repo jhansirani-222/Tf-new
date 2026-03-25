cc6de8e05d97d4b1f80643f0f2af155818bed22a
jhansi358 <jhansirani.profile@gmail.com>
2026-03-25 14:37:13 +0530

    ec2

---
+++ ec2/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "ec2" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow_all" { #tf name
+  name        = "allow_all" #aws name
+  description = "Allow all trafiic"
+  #vpc_id      = aws_vpc.main.id
+
+   ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+tags = {
+    Name = "allow_all"
+  }
+}
